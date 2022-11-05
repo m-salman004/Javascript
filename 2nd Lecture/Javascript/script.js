69de523116183e2662956e46cae201dd5ded0c04
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-05 13:33:24 -0700

    call function

--- 2nd Lecture/Javascript/script.js
+++ 2nd Lecture/Javascript/script.js
@@ -1,4 +1,9 @@
 function changeStyle () {
-   
+    document.getElementById ('styleChange').style.fontSize = '50px';
+    document.getElementById ('styleChange').style.color = '#ff0000';
+    document.getElementById ('styleChange').style.display = 'none';
+    document.getElementById ('styleChange').style.display = 'block';
 }
+
+changeStyle ();
 

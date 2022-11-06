fc6c6a2f8460438261b5a60794b3ad4f04cda056
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-05 17:58:14 -0700

    color and border

--- 2nd Lecture/Javascript/script.js
+++ 2nd Lecture/Javascript/script.js
@@ -2,9 +2,11 @@ function changeStyle () {
     // document.getElementById ("styleChange").style.display = "none";
     document.getElementById ("fontSize").style.fontSize = "50px";
     document.getElementById ("fontSize").style.border = "5px solid blue";
-    // document.getElementById ("styleChange").style.color =  "red";
+    document.getElementById ("colorBorder").style.color =  "red";
+    document.getElementById ("colorBorder").style.border =  "5px solid red";
     // document.getElementById ("styleChange").style.display = "block";
 }
+
 
 
 

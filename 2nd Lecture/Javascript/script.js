eef4b043dac0758af10006b44797c2f7fd43538e
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-05 21:39:50 -0700

    functions

--- 2nd Lecture/Javascript/script.js
+++ 2nd Lecture/Javascript/script.js
@@ -1,12 +1,19 @@
 function changeStyle () {
-    // document.getElementById ("styleChange").style.display = "none";
     document.getElementById ("fontSize").style.fontSize = "50px";
     document.getElementById ("fontSize").style.border = "5px solid blue";
+    
+}
+function colorborder () {
     document.getElementById ("colorBorder").style.color =  "red";
     document.getElementById ("colorBorder").style.border =  "5px solid red";
-    // document.getElementById ("styleChange").style.display = "block";
 }
-
+function displayNone () {
+    document.getElementById ("display").style.display = "none";
+    // document.getElementById ("display").style.display = "block";
+}
+function displayBlock () {
+    document.getElementById ("display").style.display = "block";
+}
 
 
 

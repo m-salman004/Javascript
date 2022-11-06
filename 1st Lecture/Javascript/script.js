72eec59498b73b1fa7215298107faaec7358bf7a
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-05 21:56:11 -0700

    functions

---
+++ 1st Lecture/Javascript/script.js
@@ -0,0 +1,9 @@
+function textOne () {
+    document.getElementById ('textOne'). innerHTML = 'Hello JavaScript !' ;
+}
+function bulbOn () {
+    document.getElementById('bulb').src='images/pic_bulbon.gif';
+}
+function bulbOff () {
+    document.getElementById('bulb').src='images/pic_bulboff.gif';
+}

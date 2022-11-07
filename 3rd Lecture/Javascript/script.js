e32ca71c77176f61c27f5066764b12f7f60ce907
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-07 08:12:52 +0500

    data types

--- 3rd Lecture/Javascript/script.js
+++ 3rd Lecture/Javascript/script.js
@@ -1,4 +1,17 @@
+// Alert Button
 document.getElementById ('alertString').onclick = function () {
-    alert ('Muhammad Salman !.')
-    document.getElementById ('statement').innerHTML = ''
+    alert ('Muhammad Salman !.');
+    // Clear Statement
+    document.getElementById ('statement').innerHTML = '';
+    // Clear Output
+    document.getElementById ('statement').innerHTML = '';
 }
+//    Data Types = 8
+// 1- String
+// 2- Boolean
+// 3- Number
+// 4- Array 
+// 5- Function () {}
+// 6- undefined
+// 7- Object
+// 8- Null

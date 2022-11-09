1ee06c6a46c7d908a6a50c3bec152bf3d5d635ee
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-10 00:11:19 +0500

    clear output and statement

--- 3rd Lecture/Javascript/script.js
+++ 3rd Lecture/Javascript/script.js
@@ -4,8 +4,6 @@ document.getElementById ('alertString').onclick = function () {
     // Original Statement
     let statement = "alert ('Muhammad Salman !.') "
     document.getElementById ('statement').innerHTML = statement;
-    // Clear Output
-    // document.getElementById ('Output').innerHTML = '';
 }
 
 // Alert Number
@@ -14,8 +12,7 @@ document.getElementById ('alertString').onclick = function () {
     // Original Statement
     let Number = "alert (786)"
     document.getElementById ('statement').innerHTML = Number;
-    // Clear Output
-    // document.getElementById ('Output').innerHTML = '';
+   
 }
 
 // Show Variable Names 
@@ -23,7 +20,7 @@ document.getElementById ('variableNames').onclick = function () {
     
     let Variable = ('<ul><li>Names can contain letters, digits, underscores, and dollar signs.</li></ul> <ul><li>Names must begin with a letter.</li></ul><ul><li>Names can also begin with <code> $ </code> and <code> _ </code> </li></ul> <ul><li>Names are case sensitive (y and Y are different variables).</li></ul><ul><li>Reserved words (like JavaScript keywords) cannot be used as names.</li></ul>')
     // Clear Output
-    document.getElementById ('Output').innerHTML = Variable;
+    document.getElementById ('outputs').innerHTML = Variable;
 }
 
 // Show camelCase Examples
@@ -31,9 +28,11 @@ document.getElementById ('camelCaseExamples').onclick = function () {
     
     let camelcase = ('<ul><li> camelCaseExamples </li></ul> <ul><li> variableNames </li></ul> <ul><li> alertString </li></ul> <ul><li> alertNumber </li></ul>')
     // Clear Output
-    document.getElementById ('Output').innerHTML = camelcase;
-        
-    }
+    document.getElementById ('outputs').innerHTML = camelcase;
+    
+}
+// Sum 2 Numbers
+    
 //    Data Types = 8
 // 1- String
 // 2- Boolean
@@ -43,3 +42,10 @@ document.getElementById ('camelCaseExamples').onclick = function () {
 // 6- undefined
 // 7- Object
 // 8- Null
+document.getElementById("clearstmnt").onclick = function() {
+    satement.innerText = "";
+    }
+    document.getElementById("clroutput").onclick = function() {
+    
+        outputs.innerHTML = "";
+    }

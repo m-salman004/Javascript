062a00044c38a89119018a40b63b97acfb12e977
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-12 13:09:08 +0500

    statement

--- 4th Lecture/Javascript/script.js
+++ 4th Lecture/Javascript/script.js
@@ -3,5 +3,6 @@ document.getElementById ('concatenateSomeStrings').onclick = function () {
     var message = "Good Day ";
     var name = "Salman";
     var banger = " ! ";
+    document.getElementById ('statements').innerHTML = (message + name + banger);
     
 }

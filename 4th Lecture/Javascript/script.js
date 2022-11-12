e8167dd034fc3a35aa48bb9534c21ca669acb3ef
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-12 13:12:21 +0500

    clear statement

--- 4th Lecture/Javascript/script.js
+++ 4th Lecture/Javascript/script.js
@@ -6,3 +6,7 @@ document.getElementById ('concatenateSomeStrings').onclick = function () {
     document.getElementById ('statements').innerHTML = (message + name + banger);
     document.getElementById ('outputs').innerHTML = (message + name + banger);
 }
+// Clear Statement 
+document.getElementById ('clearstmnt').onclick = function () {
+    document.getElementById ('statements').innerHTML = "";
+}

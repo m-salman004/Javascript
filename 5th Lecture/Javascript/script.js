6fa218b7ca9ca4a2fc73e72509fac7319ae58b4d
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-17 13:08:28 +0500

    variable

--- 5th Lecture/Javascript/script.js
+++ 5th Lecture/Javascript/script.js
@@ -1,4 +1,5 @@
 // Please Enter Your Name to visit our Website
 window.onload = function () {
-    
+    var message = ("Good Day !");
+    var name = prompt ("Please Enter Your Name to visit our Website.");
 }

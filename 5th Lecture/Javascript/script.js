dd15c3415928a2d4d770f38793066c3abc01d8b9
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-17 13:15:18 +0500

    heading

--- 5th Lecture/Javascript/script.js
+++ 5th Lecture/Javascript/script.js
@@ -1,7 +1,7 @@
 // Please Enter Your Name to visit our Website
-window.onload = function () {
-    var message = ("Good Day !");
-    var name = prompt ("Please Enter Your Name to visit our Website.");
+// window.onload = function () {
+//     var message = ("Good Day !");
+//     var name = prompt ("Please Enter Your Name to visit our Website.");
 
-    document.getElementById ('userName').innerHTML = (message + " " + name);
-}
+//     document.getElementById ('userName').innerHTML = (message + " " + name);
+// }

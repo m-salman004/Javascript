b1ec0be27cf37e07428241f77a325f8dd9b3cc9e
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-29 12:26:43 +0500

    if condition

--- 6th Lecture/Javascript/script.js
+++ 6th Lecture/Javascript/script.js
@@ -2,6 +2,8 @@
 window.onload = function () {
     var message = ("Good Day !");
     var name = prompt ("Please Enter Your Name to visit our Website.");
-   
+    if (name == null) {
+        alert ("Please Enter Your Name.");
+    }
     document.getElementById ('userName').innerHTML = (message + " " + name);
 };

f147215756d361a98c035abcee693981eac99138
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-29 13:05:44 +0500

    if condition and alert

--- 7th Lecture/Javascript/script.js
+++ 7th Lecture/Javascript/script.js
@@ -2,6 +2,8 @@
 window.onload = function () {
     let message = ("Good Day !");
     let name = prompt ("Please Enter your Name.");
-
+    if (name == null) { 
+        alert ("PLease Enter Your Name");
+    }
     document.getElementById ("userName").innerHTML = (message + " " + name);
  };

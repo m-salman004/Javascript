cd4e5552b75b309f8e47fc9290190673ee08af22
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-29 12:26:30 +0500

    getElementById

--- 6th Lecture/Javascript/script.js
+++ 6th Lecture/Javascript/script.js
@@ -3,5 +3,5 @@ window.onload = function () {
     var message = ("Good Day !");
     var name = prompt ("Please Enter Your Name to visit our Website.");
    
-  
+    document.getElementById ('userName').innerHTML = (message + " " + name);
 };

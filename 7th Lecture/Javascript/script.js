addd14ededf435aabc4cee67579813eafd9d2b95
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-29 13:04:07 +0500

    inner HTML

--- 7th Lecture/Javascript/script.js
+++ 7th Lecture/Javascript/script.js
@@ -2,4 +2,6 @@
 window.onload = function () {
     let message = ("Good Day !");
     let name = prompt ("Please Enter your Name.");
+
+    document.getElementById ("userName").innerHTML = (message + " " + name);
  };

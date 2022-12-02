f57ad4bd96c7510e993ace21e2b671673af1925b
Muhammad Salman <msalmantariq786@gmail.com>
2022-12-02 23:02:48 +0500

    if condition

--- 8th Lecture/Javascript/script.js
+++ 8th Lecture/Javascript/script.js
@@ -2,6 +2,8 @@
 window.onload = function () { 
     var message = ("Good Day !");
     var name = prompt("What is Your Full Name?");
-
+    if (name == null) {
+        
+    }
     document.getElementById ('userName').innerHTML = (message + " " + name);
 };

d3481fd0e09426ee91ea12d26d373d9e1b1545be
Muhammad Salman <msalmantariq786@gmail.com>
2022-12-02 23:03:11 +0500

    alert

--- 8th Lecture/Javascript/script.js
+++ 8th Lecture/Javascript/script.js
@@ -3,7 +3,7 @@ window.onload = function () {
     var message = ("Good Day !");
     var name = prompt("What is Your Full Name?");
     if (name == null) {
-        
+        alert("Please enter your full name");
     }
     document.getElementById ('userName').innerHTML = (message + " " + name);
 };

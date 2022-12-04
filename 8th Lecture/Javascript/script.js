b5850075b245217c93e1de92d9bf82c6c6f233e1
Muhammad Salman <msalmantariq786@gmail.com>
2022-12-04 22:40:55 +0500

    inner.HTML

--- 8th Lecture/Javascript/script.js
+++ 8th Lecture/Javascript/script.js
@@ -1,14 +1,16 @@
 // Please  Enter your Name 
-window.onload = function () { 
-    var message = ("Good Day !");
-    var name = prompt("What is Your Full Name?");
-    if (name == null) {
-        alert("Please enter your Full Name");
-    }   
-    document.getElementById ('userName').innerHTML = (message + " " + name);
-};
+// window.onload = function () { 
+//     var message = ("Good Day !");
+//     var name = prompt("What is Your Full Name?");
+//     if (name == null) {
+//         alert("Please enter your Full Name");
+//     }   
+//     document.getElementById ('userName').innerHTML = (message + " " + name);
+// };
 // Alternative Method
 function askName () {
         var message = "Good Day !";
         var fName   = prompt ("Please enter your Name to visit our Website.");
+
+        document.getElementById ('userName').innerHTML = (message + " " + fName );
 };

11cc63a5cf48228b0a9d47bb0604368d22c8594d
Muhammad Salman <msalmantariq786@gmail.com>
2022-12-05 23:35:40 +0500

    function

--- 6th Lecture/Javascript/script.js
+++ 6th Lecture/Javascript/script.js
@@ -1,9 +1,13 @@
 // Please Enter Your Name to visit our Website
-window.onload = function () {
-    var message = ("Good Day !");
-    var name = prompt ("Please Enter Your Name to visit our Website.");
-    if (name == null) {
-        alert ("Please Enter Your Name.");
-    }
-    document.getElementById ('userName').innerHTML = (message + " " + name);
-};
+// window.onload = function () {
+//     var message = ("Good Day !");
+//     var name = prompt ("Please Enter Your Name to visit our Website.");
+//     if (name == null) {
+//         alert ("Please Enter Your Name.");
+//     }
+//     document.getElementById ('userName').innerHTML = (message + " " + name);
+// };
+// Alternative Method
+function askName () {
+    
+}

25d5c171c59bc530871c96ef8740b7d0d9b7567f
Muhammad Salman <msalmantariq786@gmail.com>
2023-04-05 11:16:57 +0500

    Legal Variable

--- JS-01/JavaScript/app.js
+++ JS-01/JavaScript/app.js
@@ -4,3 +4,11 @@
 // let & var 
 
 let firstName = 'Muhammad Salman';
+
+// Variable Rules
+// 1. Legal or Illegal names
+// Legal
+// let lastName // CamelCase
+// lastname
+// Allowed Special Characters
+// _ $

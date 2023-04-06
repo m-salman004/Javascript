7b3e6aa809d5250ae44d0b2330adcf081d755c14
Muhammad Salman <msalmantariq786@gmail.com>
2023-04-06 17:15:37 +0500

    String addition

--- JS-02/JavaScript/app.js
+++ JS-02/JavaScript/app.js
@@ -5,3 +5,15 @@ let data1 = 'Hello ';
 let data2 = 'Salman';
 let data3 = data1 + data2;
 console.log(data3); 
+
+// string + number = concatenation
+// number + string = concatenation
+// string + string = concatenation
+let data4 = '5';
+let data5 = 6;
+console.log(data4 + data5);
+
+// String Addition
+let data6 = 5;
+let data7 = 9;
+console.log(data6 + data7);

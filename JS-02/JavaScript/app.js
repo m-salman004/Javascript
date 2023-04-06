82625b5e748cda782decb3af0ebe6a7ebf0a22c8
Muhammad Salman <msalmantariq786@gmail.com>
2023-04-06 13:00:59 +0500

    String Concatenation

--- JS-02/JavaScript/app.js
+++ JS-02/JavaScript/app.js
@@ -1,1 +1,7 @@
-let data = '';  // Empty string
+// let data = '';  // Empty string
+// Variable
+// String Concatenation
+let data1 = 'Hello ';
+let data2 = 'Salman';
+let data3 = data1 + data2;
+console.log(data3); 

9bcd77e2eb00dfb42cf488d3a5e3dd0754a09cc7
Muhammad Salman <msalmantariq786@gmail.com>
2023-04-07 14:24:46 +0500

    If else

--- JS-03/JavaScript/app.js
+++ JS-03/JavaScript/app.js
@@ -7,9 +7,17 @@
 // Greater Than Equals to
 
 // If Else
-let userAge = +prompt('What is your Age?');
-if (userAge >= 20) {
-    console.log('You are Eligible for NIC');
-} else {
-    console.log('You are not Eligible for NIC');
+// let userAge = +prompt('What is your Age?');
+// if (userAge >= 20) {
+//     console.log('You are Eligible for NIC');
+// } else {
+//     console.log('You are not Eligible for NIC');
+// }
+
+let userFavNumber = prompt('Enter a number?');
+let computeNumber = Math.round (Math.random () * 10);
+if (userFavNumber === computeNumber) { 
+    console.log('You Won!');
+} else { 
+    console.log('You Lose! ' + computeNumber);
 }
